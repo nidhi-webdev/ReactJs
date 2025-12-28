8325147f75f2028d7642e8a9d7bb5f83bf3a310c
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-28 18:59:52 +0100

    Structure

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -9,6 +9,8 @@ const App = () => {
     <div className='parent'>
       <div className='card'>
         <h1> Nidhi </h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!</p>
+      <button>View Profile </button>
       </div>
     </div>
   )
