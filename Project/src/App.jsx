b45924fb6ddc8e361899dc727df6acb736e3e1a3
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-28 19:28:10 +0100

    Passing data through Props

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -3,12 +3,13 @@ import Card from './Card'
 
 const App = () => {
 
-  const name = "Nidhi"
-  const age = 20
+ 
 
   return (
     <div className='parent'>
-     <Card />
+     <Card name="Nidhi" img="https://images.unsplash.com/photo-1766860891078-3aeb5647953f?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+     <Card name="Sunidhi" img="https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='Normal image' />
+
     </div>
   )
 }
