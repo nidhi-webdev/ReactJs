37357dae55fef7c0ca6152d6bbffbddf74f8bdfa
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-28 19:06:58 +0100

    Structure2

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div className='parent'>
       <div className='card'>
+        <img src='https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Normal image'/>
         <h1> Nidhi </h1>
         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!</p>
       <button>View Profile </button>
