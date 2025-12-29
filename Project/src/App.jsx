bc4635cbbc886e96d6e8d3e3848df21be9faac2e
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-29 21:32:49 +0100

    Data is not visible yet

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -5,13 +5,13 @@ const App = () => {
 
   const data = [
     {
-      name: Nidhi,
+      name: "Nidhi",
       image: "https://images.unsplash.com/photo-1766860891078-3aeb5647953f?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!"
 
     },
     {
-      name: Sunidhi,
+      name: "Sunidhi",
       image: "https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!"
 
@@ -21,8 +21,11 @@ const App = () => {
 
   return (
     <div className='parent'>
-      <Card />
-      <Card />
+      {data.map(function (elem) {
+        <Card />
+      })}
+
+
 
     </div>
   )
