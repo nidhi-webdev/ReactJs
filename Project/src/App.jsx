aaae63413d8994d516a9c280bb2bf2d8459b5ba6
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-29 21:31:08 +0100

    Trying to get the data form arr

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -3,12 +3,26 @@ import Card from './Card'
 
 const App = () => {
 
- 
+  const data = [
+    {
+      name: Nidhi,
+      image: "https://images.unsplash.com/photo-1766860891078-3aeb5647953f?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!"
+
+    },
+    {
+      name: Sunidhi,
+      image: "https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!"
+
+    }
+
+  ]
 
   return (
     <div className='parent'>
-     <Card name="Nidhi" img="https://images.unsplash.com/photo-1766860891078-3aeb5647953f?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-     <Card name="Sunidhi" img="https://images.unsplash.com/photo-1761839258513-099c3121d72d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='Normal image' />
+      <Card />
+      <Card />
 
     </div>
   )
--- Project/src/Card.jsx
+++ Project/src/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Card = (props) => {
-  return (
-    <div className='card'>
-        <img src={props.img} alt='Normal image'/>
-        <h1> {props.name} </h1>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis commodi eum! Nesciunt, neque eveniet. Optio neque nostrum aut eum a quae adipisci repudiandae ad aperiam voluptatem! Quibusdam, soluta. Totam!</p>
-      <button>View Profile </button>
-      </div>
-  )
+    return (
+        <div className='card'>
+            <img src={props.img} alt='Normal image' />
+            <h1> {props.name} </h1>
+            <p> {props.text} </p>
+            <button>View Profile </button>
+        </div>
+    )
 }
 
 export default Card
