fb4670764f8408c91eda415fae42c00c47f26626
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-30 12:54:57 +0100

    The best way

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,17 +1,21 @@
 import './index.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
 
   const inputChnage = (val) => {
+    setInputValue(val);
     console.log(val);
 
   }
 
   return (
     <div>
-      <input placeholder='Enter Name' onChange={(elem) => {
-        inputChnage(elem.target.value)
-      }} />
+      <input placeholder='Enter Name' value={inputValue}
+        onChange={(e) => {
+          inputChnage(e.target.value)
+        }} />
     </div>
   )
 }
