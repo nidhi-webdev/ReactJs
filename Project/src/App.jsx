9b61fde852f064b080b587ec185a8d840e66ea15
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-30 13:39:57 +0100

    On onclick consoling the value

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const App = () => {
-const a = 20;
+  const a = 20;
+
+  const changeValue = (val) => {
+    console.log("val");
+
+  }
 
 
   return (
     <div>
       <h1 className='text-black'> Value of a is {a} </h1>
-      <button>
+      <button className='w-32'
+        onClick={changeValue}
+      >
         click
       </button>
     </div>
