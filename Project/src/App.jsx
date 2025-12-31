aeb42df1674b810da43f7a2a8755c7e779b9d42a
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-31 12:58:30 +0100

    Made function and consoling

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
 const App = () => {
+  const [num, setNum] = useState("2")
+
+
+
+  const Increase = () => {
+    console.log("Increasing");
+
+  }
+  const Decrease = () => {
+    console.log("Decreasing")
+
+  }
+
   return (
     <div>
-      <h1> 0 </h1>
-      <button> Increase </button>
-      <button> Decrease </button>
+      <h1> {num} </h1>
+      <button onClick={Increase}> Increase </button>
+      <button onClick={Decrease}> Decrease </button>
     </div>
   )
 }
