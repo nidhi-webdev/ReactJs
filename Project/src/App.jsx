9948679be9b7daf171a252cd089dca0bafe71464
nidhi-webdev <kumarinidhi0594@gmail.com>
2025-12-31 12:42:39 +0100

    Trying to make the counter

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import './index.css'
 
 const App = () => {
   return (
     <div>
+      <h1> 0 </h1>
       <button> Increase </button>
       <button> Decrease </button>
     </div>
