1411b8dcfbe7ad718fe467466cc9cf79a1fbd3d6
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-01 22:37:55 +0100

    Making some project to understand form handling better

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
 
 const App = () => {
   return (
-    <div className=''>
+    <div className='h-screen'>
 
-      <form className='flex items-center'> 
-        <input type='text' placeholder='Enter Notes Heading' />
-        <textarea placeholder='Enter Details'> </textarea>
+      <form className='flex p-10 items-start flex-col gap-4'> 
+        <input 
+        type='text' 
+        placeholder='Enter Notes Heading' 
+        className=' border-2 rounded-xl p-2'
+        />
+        <textarea 
+        className='border-2  rounded-xl h-32 p-2'
+        placeholder='Enter Details' 
+        name='' 
+        id='' 
+        defaultValue="" />
       </form>
     </div>
   )
