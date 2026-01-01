6bbf5072cb5b9179687585578f9a6723c9deac83
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-01 21:03:53 +0100

    Prevent Default

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted");
+
+  }
+
+
   return (
     <div>
-      <form>
-        <input type='text' />
-        <input type='text' />
-        <button></button>
+      <form onSubmit={(e) => {
+        handleSubmit(e)
+      }}>
+        <input type='text' placeholder='Enter Your Name' />
+
+        <button> Submit </button>
       </form>
     </div>
   )
