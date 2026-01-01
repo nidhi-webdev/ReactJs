26911f30129638a8a9ab237fdce003787b6ed16c
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-01 22:44:01 +0100

    Design part

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,16 +1,15 @@
-
 const App = () => {
   return (
     <div className='h-screen'>
 
-      <form className='flex p-10 items-start flex-col gap-4'> 
+      <form className='flex p-10 items-start flex-col gap-4 w-full'> 
         <input 
         type='text' 
         placeholder='Enter Notes Heading' 
-        className=' border-2 rounded-xl p-2'
+        className='border-2 rounded-xl p-2 w-full'
         />
         <textarea 
-        className='border-2  rounded-xl h-32 p-2'
+        className='border-2 rounded-xl h-32 p-2 w-full'
         placeholder='Enter Details' 
         name='' 
         id='' 
