ccd7aaf6c98ea05d5099a271c3c3e2c1e912a735
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-02 13:48:49 +0100

    Data binding

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,30 +1,45 @@
+import { useState } from "react"
+
 const App = () => {
+  const [tittle, setTittle] = useState(" ")
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log("Hello");
-
+    setTittle("")
   }
 
   return (
     <div className='h-screen bg-black text-white flex'>
+
       <form className='flex p-10 items-start flex-col gap-4 bg-black w-1/2' onClick={(e) => { handleSubmit(e) }}>
-      <h1 className="text-3xl"> Add Notes </h1>
+
+        <h1 className="text-3xl"> Add Notes </h1>
+
         <input
+          value={tittle}
+          onChange={(e) => {
+            setTittle(e.target.value)
+          }}
           type='text'
           placeholder='Enter Notes Heading'
           className='border-2 rounded-xl p-2 w-full'
         />
+
         <textarea
           className='border-2 rounded-xl h-32 p-2 w-full'
           placeholder='Enter Details'
           name=''
           id=''
           defaultValue="" />
+
         <button className="bg-white text-black w-full px-5 py-3 rounded-xl cursor-pointer"> Add Notes </button>
+
       </form>
+
       <div className="bg-gray-800 w-1/2 mt-10 p-4">
+
         <h1 className="ml-10 text-3xl"> Your Notes </h1>
+
         <div className="flex flex-wrap mt-5 gap-5 overflow-auto">
           <div className="h-52 w-40 rounded-2xl bg-white   "></div>
           <div className="h-52 w-40 rounded-2xl bg-white  "></div>
@@ -33,6 +48,7 @@ const App = () => {
           <div className="h-52 w-40 rounded-2xl bg-white  "></div>
           <div className="h-52 w-40 rounded-2xl bg-white  "></div>
         </div>
+
       </div>
     </div>
   )
