0948a9ce98a6de56e82954cf39873d99561a8958
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-03 17:24:21 +0100

    Detele Icon

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -53,8 +53,10 @@ const App = () => {
         <div className="flex flex-wrap mt-5 gap-5 overflow-auto">
 
           {task.map((elem, idx) => {
-            return <div className="bg-cover h-52 w-40 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] " key={idx}>
-              <h2> <X /> </h2>
+            return <div className=" relative bg-cover h-52 w-40 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] " key={idx}>
+              <h2 className="text-black absolute top-5 right-5 bg-red-500 p-1 rounded-full">
+                <X strokeWidth={1.75} />
+              </h2>
               <h3 className="leading-tight pt-10 ml-5 text-black font-bold">{elem.tittle}</h3>
               <p className="leading-tight font-medium ml-5 text-gray-600">{elem.detail}</p>
             </div>
