455b5632ec7b1e8cb4bdbcde9e1d2ccbad08fb05
nidhi-webdev <kumarinidhi0594@gmail.com>
2026-01-07 12:18:08 +0100

    First I made 2 usestate

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 
 const App = () => {
 const [num, setNum] = useState(0)
+const [num2, setNum2] = useState(100)
 
 const count = () => {
   setNum(num + 1)
+  setNum2(num + 10)
 }
 
 useEffect(() => {
@@ -15,7 +17,8 @@ useEffect(() => {
 
   return (
     <div className="p-10">
-      <h1 className="bold"> {num} </h1>
+      <h1 className="bold">Num: {num} </h1>
+       <h1 className="bold"> Num2 :{num2} </h1>
       <button onClick={count} className='bg-amber-500 text-white p-3 rounded-2xl'> Click me </button>
     </div>
   )
